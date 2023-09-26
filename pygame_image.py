992537431f5fc6bd609ec463de1647fad9ab105f
Akai Shogo <c0a22003a7@edu.teu.ac.jp>
2023-09-26 17:18:16 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -10,7 +10,16 @@ def main():
     kk_img= pg.image.load("ex01-20230926/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_imgs = pg.transform.rotozoom(kk_img, 10, 1.0)
-    kk_imgs = [kk_img, kk_imgs]
+    kk_image3 = pg.transform.rotozoom(kk_img, 3, 1.0)
+    kk_image4 = pg.transform.rotozoom(kk_img, 4, 1.0)
+    kk_image5 = pg.transform.rotozoom(kk_img, 5, 1.0)
+    kk_image6 = pg.transform.rotozoom(kk_img, 6, 1.0)
+    kk_image7 = pg.transform.rotozoom(kk_img, 7, 1.0)
+    kk_image8 = pg.transform.rotozoom(kk_img, 8, 1.0)
+    kk_image9 = pg.transform.rotozoom(kk_img, 9, 1.0)
+    kk_image10 = pg.transform.rotozoom(kk_img, 10, 1.0)
+    kk_imgs = [kk_img, kk_imgs, kk_image3,kk_image4, kk_image5, kk_image6, kk_image7, kk_image8, kk_image9, kk_image10]
+    #kk_imgs = [kk_img, kk_imgs]
     tmr = 0
     while True:
         for event in pg.event.get():
